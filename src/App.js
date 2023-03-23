cd65c8f970a1147568b2ff9469922570ab62be65
hanomer18 <96796878+hanomer18@users.noreply.github.com>
2023-03-23 13:53:10 +0300

    sadasdasd

---
+++ src/App.js
@@ -0,0 +1,27 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <img src="/logo192.png" alt="" />
+      <header className="App-header">
+
+
+        <p>
+          Edit <code></code> and save to reload
+
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
